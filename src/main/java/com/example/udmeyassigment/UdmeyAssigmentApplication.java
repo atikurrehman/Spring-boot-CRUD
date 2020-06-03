c94637bb7b43846d3350523f7f39f6087568f1c6
atikurrehman <atikurrehmank@gmail.com>
2020-06-03 19:16:36 +0530

    init the project from spring initilizer

---
+++ src/main/java/com/example/udmeyassigment/UdmeyAssigmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.udmeyassigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdmeyAssigmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdmeyAssigmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/udmeyassigment/UdmeyAssigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udmeyassigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdmeyAssigmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
