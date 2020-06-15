11e7d1bc1351d7a22508ccf22f49c2c764f0e508
atikurrehman <atikurrehmank@gmail.com>
2020-06-15 15:40:17 +0530

    added spring cloud config properties

---
+++ src/test/java/com/example/udmeyassigment/controller/CustomerControllerTest.java
@@ -0,0 +1,38 @@
+package com.example.udmeyassigment.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.customerservice.controller.CustomerController;
+
+class CustomerControllerTest {
+
+	@Autowired
+	CustomerController customerController;
+
+	@Test
+	void testGetCustomerById() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateCustomer() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateCustomer() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteCustomerById() {
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	void testCustomerController() {
+		// fail("Not yet implemented");
+	}
+
+}
